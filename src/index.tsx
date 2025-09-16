9776bd496a27f2630efe850b12fb06ea53fb876a
ryncolorgen-code <ryncolorgen@gmail.com>
2025-09-16 17:10:14 +0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
-
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
