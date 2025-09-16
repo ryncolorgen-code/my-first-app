a4f5b7f533333d1f30f02e69fc84572a5bc42cc8
ryncolorgen-code <ryncolorgen@gmail.com>
2025-09-16 14:31:40 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import path from 'path';
 
-export default defineConfig({
-  plugins: [react()],
-  base: './'
+import { defineConfig, loadEnv } from 'vite';
+
+
+
+export default defineConfig(({ mode }) => {
+
+    const env = loadEnv(mode, '.', '');
+
+    return {
+
+      define: {
+
+        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+
+      },
+
+      resolve: {
+
+        alias: {
+
+          '@': path.resolve(__dirname, '.'),
+
+        }
+
+      }
+
+    };
+
 });
